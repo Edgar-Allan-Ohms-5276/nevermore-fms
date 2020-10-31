4296e1c662f94d25f10bce05a2b32634de514e28
Chase M <mcmackety@rodiconmc.com>
2020-10-30 21:36:01 -0400

    Removed cache

--- backend/config/prod.exs
+++ backend/config/prod.exs
@@ -11,7 +11,6 @@ use Mix.Config
 # before starting your production server.
 config :nevermore, NevermoreWeb.Endpoint,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :warn
